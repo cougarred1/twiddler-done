3f7bfe0c4e6a8ec9e017ca0865986359b7d0ef99
Rodman Lavalais <RodmanLavalais@gmail.com>
2024-02-27 19:55:50 -0600

    start

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 
 $(document).ready(() => {
   const $body = $('body');
-  $body.html('');
+  $body.html(''); //clears the body
 
-  const $tweets = streams.home.map((tweet) => {
+    const $tweetsDiv = $('<div id=tweets>');
+    $body.append($tweetsDiv);
+    const $tweets = streams.home.map((tweet) => {
     const $tweet = $('<div></div>');
     const text = `@${tweet.user}: ${tweet.message}`;
+    
+    function makeNewTweets(){
+        
+    }
 
     $tweet.text(text);
 
     return $tweet;
   });
-  $body.append($tweets);
+  $tweetsDiv.append($tweets);
 
 });
